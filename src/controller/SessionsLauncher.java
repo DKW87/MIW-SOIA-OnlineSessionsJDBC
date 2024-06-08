5a97d98cd7c8a2a2dde4760cfe53fade3dedbc52
Danny Kwant <danny.kwant@hva.nl>
2024-06-08 16:21:25 +0200

    9.8 Controle op foreign key constraint

--- src/controller/SessionsLauncher.java
+++ src/controller/SessionsLauncher.java
@@ -12,16 +12,24 @@ public class SessionsLauncher {
     public static void main(String[] args) {
 
         DBaccess dBaccess =
-                new DBaccess("OnlineSessions","userOnlineSessions","userOnlineSessionsPW");
+                new DBaccess("OnlineSessions", "userOnlineSessions", "userOnlineSessionsPW");
         dBaccess.openConnection();
-
         SessionDAO sessionDAO = new SessionDAO(dBaccess);
+        MuzikantDAO muzikantDAO = new MuzikantDAO(dBaccess);
+
+        Muzikant miley = new Muzikant("Miley Cyrus", "Zang", 15);
+
+        if (muzikantDAO.getMuzikantPerNaam("Miley Cyrus") == null) {
+            muzikantDAO.slaMuzikantOp(miley);
+        }
+
+        sessionDAO.slaSessionOp(new Session(miley, LocalDate.now(), 3, false, 7));
         List<Session> sessionsInDeDatabase = sessionDAO.getSessions();
-        dBaccess.closeConnection();
 
         for (Session session : sessionsInDeDatabase) {
             System.out.println(session);
         }
+        dBaccess.closeConnection();
 
     } // main
 
